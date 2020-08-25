bea36958ea182a8f6756cf56c7034e2ea57faca8
Dyobi <dyobi@Dyobi.local>
2020-08-24 20:42:52 -0700

    .

--- sources/screens/nav/map/index.jsx
+++ sources/screens/nav/map/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 const Map = () => {
     return (
         <View style={styles.container}>
-            <Text>Map</Text>
+            <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }}
+            />
         </View>
     );
 }
